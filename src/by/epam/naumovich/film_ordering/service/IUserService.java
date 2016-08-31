66c66931405312b878b6f18198dc951de5ddfd50
Dmitry Naumovich <dmitry.nmv@gmail.com>
2016-09-01 02:56:07 +0300

    addUser and updateUser method input parameters added

--- src/by/epam/naumovich/film_ordering/service/IUserService.java
+++ src/by/epam/naumovich/film_ordering/service/IUserService.java
@@ -5,8 +5,12 @@ import by.epam.naumovich.film_ordering.service.exception.ServiceException;
 
 public interface IUserService {
 	
-	void addUser(User user) throws ServiceException;
-	void updateUser(int id, User user) throws ServiceException;
+	void addUser(String login, String name, String surname, String password, 
+			String sex, String bDate, String phone, String email, String about, String avatar) throws ServiceException;
+	
+	void updateUser(int id, String name, String surname, String password, 
+			String sex, String bDate, String phone, String email, String about, String avatar) throws ServiceException;
+	
 	User getUserByLogin(String login) throws ServiceException;
 	void checkUserPassword(String login, String password) throws ServiceException;
 	String getLoginByID(int id) throws ServiceException;
