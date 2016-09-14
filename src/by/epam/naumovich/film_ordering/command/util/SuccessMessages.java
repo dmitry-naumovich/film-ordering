af243bbb7d3244a03d41fde6d1da50c349ed144c
Dmitry Naumovich <dmitry.nmv@gmail.com>
2016-09-14 13:58:12 +0300

    SuccessMessage about ordering film changed a bit

--- src/by/epam/naumovich/film_ordering/command/util/SuccessMessages.java
+++ src/by/epam/naumovich/film_ordering/command/util/SuccessMessages.java
@@ -10,7 +10,7 @@ public final class SuccessMessages {
 	public static final String NEWS_ADDED = "News was successfully added";
 	public static final String NEWS_EDITED = "News was successfully edited";
 	public static final String NEWS_DELETED = "News was successfully deleted";
-	public static final String ORDER_ADDED = "You have successfully ordered this film";
+	public static final String ORDER_ADDED = "You have successfully purchased this film";
 	public static final String REVIEW_ADDED = "Congratulations! Your review was successfully added";
 	public static final String REVIEW_DELETED = "The review was successfully deleted";
 	public static final String SETTINGS_UPDATED = "Your profile settings were successfully updated";
