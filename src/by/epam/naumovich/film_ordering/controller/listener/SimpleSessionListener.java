acd9614ea9e1e1126c22b8dff8bdb6a3471161ef
Dmitry Naumovich <dmitry.nmv@gmail.com>
2016-09-20 12:31:32 +0300

    session listeners added to the project

---
+++ src/by/epam/naumovich/film_ordering/controller/listener/SimpleSessionListener.java
@@ -0,0 +1,25 @@
+package by.epam.naumovich.film_ordering.controller.listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SimpleSessionListener implements HttpSessionListener {
+
+	private static final Logger logger = LogManager.getLogger(Logger.class.getName());
+	private static final String SESSION_CREATED = "Session created: jsessionID = ";
+	private static final String SESSION_DESTROYED = "Session destroyed: jsessionID = ";
+	
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0) {
+		logger.debug(SESSION_CREATED + arg0.getSession().getId());
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		logger.debug(SESSION_DESTROYED + arg0.getSession().getId());
+	}
+
+}
